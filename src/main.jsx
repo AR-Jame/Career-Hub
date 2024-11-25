057004d32b500331f8681b24a9223338362cb4b0
AR-Jame <ar.jame333@gmail.com>
2024-11-25 22:50:56 +0600

    kjfakljdfklj

--- src/main.jsx
+++ src/main.jsx
@@ -10,28 +10,28 @@ import Jobs from './Components/JobsFolder/Jobs/Jobs.jsx';
 import Form from './Components/Form/Form.jsx';
 import Home from './Components/Home/Home.jsx';
 import JobDetails from './Components/JobDetails/JobDetails.jsx';
-import ErrorPage from './ErrorPage.jsx';
+import Error from './Components/Error/Error.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
-    errorElement:<ErrorPage />,
+    errorElement:<Error></Error>,
     children:[
       {
         path: "/",
         element:<Home></Home>,
-        loader:() => fetch("../public/data/categories.json"),
+        loader:() => fetch("./data/categories.json"),
       },
       {
         path:"/job/:Id",
         element: <JobDetails></JobDetails>,
-        loader: () => fetch("../public/data/jobs.json")
+        loader: () => fetch("./data/jobs.json")
       },
       {
         path: "/jobs",
         element: <Jobs></Jobs>,
-        loader: () => fetch("../public/data/jobs.json")
+        loader: () => fetch("./data/jobs.json")
       },
       {
         path: "/form",
